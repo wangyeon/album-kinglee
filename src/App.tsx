e867c7b67ef7f84f0ee0174364e49658b3762ce3
Kinglee <mirr2336@gmail.com>
2025-12-02 21:24:38 +0900

    key in

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+//import { useState } from 'react'
 import TodoBoard from './components/TodoBoard'
 function App() {
+    //const [inputValue, setInputValue]=useState<string>()
+
     return (
         <div>
-            <input type="text" />
+            <input type="text" onChange={(event) => console.log(event.target.value)} />
             <button>추가</button>
             <TodoBoard />
         </div>

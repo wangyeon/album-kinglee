ed5cf43dc8bc78736120f6fd5bddfeb100e49985
Kinglee <mirr2336@gmail.com>
2025-12-03 20:39:00 +0900

    111

--- src/components/common/searchBar/CommonSearchBar.tsx
+++ src/components/common/searchBar/CommonSearchBar.tsx
@@ -6,7 +6,7 @@ function CommonSearchBar() {
         <div className={styles.searchBar}>
             <div className={styles.searchBar__search}>
                 <input type="text" placeholder="찾으실 이미지를 검색하세요." className={styles.searchBar__search__input} />
-                <img src="https://wangyeon.github.io/album-kinglee/origin/src/assets/icons/icon-search.svg" alt="" />
+                <img src="/src/assets/icons/icon-search.svg" alt="" />
             </div>
         </div>
     )
